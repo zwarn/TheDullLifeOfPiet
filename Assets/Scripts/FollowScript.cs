ae3b67d999d397d2e9dde437e1872fd16eeb0451
zwarn <michael.budahn@gmail.com>
2014-12-13 17:41:06 +0100

    basic parallex

--- Assets/Scripts/FollowScript.cs
+++ Assets/Scripts/FollowScript.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class FollowScript : MonoBehaviour {
 
 	public GameObject toFollow;
 	public float camSpeed = 0.6f;
+
+	public List<ParallexScrollingEntry> parallex;
+
+	[System.Serializable]
+	public class ParallexScrollingEntry {
+		public GameObject plane;
+		public Vector3 scrollSpeed;
+
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -13,5 +23,9 @@ public class FollowScript : MonoBehaviour {
 		                             transform.position.z);
 		//transform.position = Vector3.Lerp (transform.position, target, camSpeed * Time.deltaTime);
 		transform.position = target;
+
+		foreach (ParallexScrollingEntry entry in parallex) {
+			entry.plane.transform.position = Vector3.Scale (toFollow.transform.position, entry.scrollSpeed);
+		}
 	}
 }
---
+++ Assets/Scripts/ScreenResetScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenResetScript : MonoBehaviour {
+
+	public Rect bounds;
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < bounds.xMin) {
+			transform.Translate(new Vector3(bounds.width,0,0));
+		}
+		if (transform.position.x > bounds.xMax) {
+			transform.Translate(new Vector3(-bounds.width,0,0));
+		}
+		if (transform.position.y < bounds.yMin) {
+			transform.Translate(new Vector3(0,bounds.height,0));
+		}
+		if (transform.position.y > bounds.yMax) {
+			transform.Translate(new Vector3(0,-bounds.height,0));
+		}
+	}
+}
