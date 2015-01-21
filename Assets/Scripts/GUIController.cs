f188bf2a86b4c44b32a971545b8785c8ba45ad4c
projektorkraft <projektorkraft@gmail.com>
2015-01-21 07:26:37 +0100

    GUI and Points

---
+++ Assets/Scripts/GUIController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIController : MonoBehaviour {
+
+	public Canvas gui;
+
+	private GameController gameController;
+	private Text pointsLabel;
+
+	void Start () {
+		gameController = GameController.Instance;
+		pointsLabel = gui.transform.FindChild ("Points").GetComponent<Text> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		pointsLabel.text = ((int) gameController.points).ToString();
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,15 +5,20 @@ public class GameController : MonoBehaviour {
 
 	private static GameController _instance;
 
+	public float points;
+
 	private LevelCreator levelCreator;
 	private PlayerController playerController;
+	private CactusController cactusController;
+
+	private float highestX = 0;
 
 	void Awake ()
 	{
 		_instance = this;
 	}
 	
-	public GameController Instance {
+	public static GameController Instance {
 		get {return _instance;}
 	}
 	// Use this for initialization
@@ -22,10 +27,16 @@ public class GameController : MonoBehaviour {
 		levelCreator.resetMap ();
 
 		playerController = PlayerController.Instance;
+
+		cactusController = CactusController.Instance;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float x = playerController.gameObject.transform.position.x;
+		if (x > highestX) {
+			points += (x - highestX) * Mathf.Pow(2,1+cactusController.cactusLevel);
+			highestX = x;
+		}
 	}
 }
